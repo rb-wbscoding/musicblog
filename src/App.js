7e966ca65c5a80133f063b8dd3a72751c10ead9a
Jakub Jureczko <untech.untech@gmail.com>
2020-08-17 17:35:15 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -28,12 +28,24 @@ function App() {
       <h1>{entry.fields.title}</h1>
       <img src={entry.fields.image.fields.file.url} alt="" />
       <section
+        className="description"
         dangerouslySetInnerHTML={{ __html: marked(entry.fields.description) }}
       />
     </div>
   ));
 
-  return <div className="App">{Entries}</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <header>
+          <div className="head"></div>
+        </header>
+        <main>
+          <div className="mainwrapper">{Entries}</div>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
