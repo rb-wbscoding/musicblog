49809be89c9878da037a3625de7e490790d16457
Jakub Jureczko <untech.untech@gmail.com>
2020-08-17 15:55:33 +0200

    gitignore

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import * as Contentful from "contentful";
+import marked from "marked";
+
+const client = Contentful.createClient({
+  space: process.env.REACT_APP_SPACE_ID,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    client
+      .getEntries({
+        content_type: "recipies",
+      })
+      .then((response) => {
+        console.log(response);
+        setEntries(response.items);
+      });
+  }, []);
+  //const postDescription = marked(entry.fields.description)
+  //<section dangerouslySetInnerHTML={{ __html: postDescription }} />
+  const Entries = entries.map((entry) => (
+    <div className="entry" key={entry.sys.id}>
+      <h1>{entry.fields.name}</h1>
+      <img src={entry.fields.feature.fields.file.url} alt="" />
+      <section
+        dangerouslySetInnerHTML={{ __html: marked(entry.fields.description) }}
+      />
     </div>
-  );
+  ));
+
+  return <div className="App">{Entries}</div>;
 }
 
 export default App;
